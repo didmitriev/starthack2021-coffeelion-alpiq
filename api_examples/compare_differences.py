8f0c54cb896f35614f2f52b5040eb2c5866d0450
Micha Burkhardt <micha.burkhardt@uni-oldenburg.de>
2021-03-20 17:40:48 +0100

    intraday and da price plotting

--- api_examples/compare_differences.py
+++ api_examples/compare_differences.py
@@ -5,28 +5,27 @@ from matplotlib import pyplot as plt
 # authentification
 session = wapi.Session(client_id='9-k2UK_O.nj6dzxl1gpSfhwJUbxI7mv8', client_secret='eSv3Xk3W-M.A.NrP.i1onr0k-Pf5UMhNxm4.CU7aOFPVUSZI0IYeLF4dpBl5_-HUr7Sws.iUSwHf7bcSxUOfPCm032W_Q8PwZRPQ')
 
-# set the dates
+# set the date to start from 0:00 this day
 start_date = pd.Timestamp.now().floor('D')
 
 ############################
 
-### Get daily average price
-curve = session.get_curve(name='pri de spot €/mwh cet min15 a') # which curve to get
+### Get intraday prices
+curve = session.get_curve(name='pri de intraday vwap id1 €/mwh cet h a') # which curve to get
 ts = curve.get_data(data_from=start_date) # date of the curve
-da_price = ts.to_pandas() # convert TS object to pandas.Series object (for plotting)
+intraday_price = ts.to_pandas()[0:24] # convert TS object to pandas.Series object (for plotting)
 
-### Get predicted price
-curve = session.get_curve(name='pri de spot ec00 €/mwh cet h f')
-ts = curve.get_latest()
-#ts = curve.get_instance(issue_date=yesterday)
-predicted_da_price = ts.to_pandas() 
+### Get day ago price
+curve = session.get_curve(name='pri de spot €/mwh cet h a') # which curve to get
+ts = curve.get_data(data_from=start_date) # date of the curve
+da_price = ts.to_pandas()[0:24] # convert TS object to pandas.Series object (for plotting)
 
 
 ### WIND 
 # PREDICRION
 curve = session.get_curve(name='pro de wnd ec00 mwh/h cet min15 f')
 ts = curve.get_instance(issue_date=start_date)
-wind_pred = ts.to_pandas()
+wind_pred = ts.to_pandas()[0:96]
 # ACTUAL
 curve = session.get_curve(name='pro de wnd mwh/h cet min15 a')
 ts = curve.get_data(data_from=start_date)
@@ -36,7 +35,7 @@ wind_actual = ts.to_pandas()
 # PREDICITION
 curve = session.get_curve(name='pro de spv ec00 mwh/h cet min15 f')
 ts = curve.get_instance(issue_date=start_date)
-solar_pred = ts.to_pandas()
+solar_pred = ts.to_pandas()[0:96]
 # ACTUAL
 curve = session.get_curve(name='pro de spv mwh/h cet min15 a')
 ts = curve.get_data(data_from=start_date)
@@ -44,7 +43,7 @@ solar_actual = ts.to_pandas()
 print(wind_actual)
 
 combined_renewables = wind_actual + solar_actual
-predicted_renewables = wind_pred[0:96] + solar_pred[0:96]
+predicted_renewables = wind_pred + solar_pred
 
 
 ### PLOTTING
@@ -52,12 +51,12 @@ fig, ax = plt.subplots(2,1)
 
 ax[0].plot(combined_renewables, label="actual renewable energy")
 ax[0].plot(predicted_renewables, label="predicted renewable energy")
-ax[0].legend(loc='upper right')
+ax[0].legend(loc='lower right')
 ax[0].set_ylabel("MWH")
 
-ax[1].plot(da_price, label="actual price")
-#ax[1].plot(predicted_da_price, label="predicted price")
-ax[1].legend(loc='upper right')
+ax[1].plot(intraday_price, label="intraday price")
+ax[1].plot(da_price, label="day ago price")
+ax[1].legend(loc='lower right')
 ax[1].set_ylabel("€/MWH")
 
 plt.show()
